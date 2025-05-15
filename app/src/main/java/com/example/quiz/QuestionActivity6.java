77a793927913ec639c908f7b9ab80ed1a696831f
Arthi Shanmugam <arthi24012005@gmail.com>
2025-05-15 11:26:31 +0530

    Update QuestionActivity6.java

--- app/src/main/java/com/example/quiz/QuestionActivity6.java
+++ app/src/main/java/com/example/quiz/QuestionActivity6.java
@@ -75,7 +75,7 @@ public class QuestionActivity6 extends AppCompatActivity {
 
         questionnumber = findViewById(R.id.DispName);
         submitbutton = findViewById(R.id.button3);
-        submitbutton = findViewById(R.id.buttonquit);
+        quitbutton = findViewById(R.id.buttonquit);
         tv = findViewById(R.id.tvque);
 
 
